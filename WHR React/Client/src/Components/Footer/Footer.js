5373142dab53afa289673d11920907c95ab36f89
Nivedhidha-I <71599134+Nivedhidha-I@users.noreply.github.com>
2026-01-11 05:58:29 +0100

    Update Footer.js

--- WHR React/Client/src/Components/Footer/Footer.js
+++ WHR React/Client/src/Components/Footer/Footer.js
@@ -8,14 +8,6 @@ function Footer(){
                 <a href="https://github.com/Nivedhidha-I/World-Happiness-Report"><i class="icons fab fa-github fa-3x"/></a>
                 <a href="https://www.kaggle.com/mathurinache/world-happiness-report?select=2020.csv"><i class="icons fab fa-kaggle fa-3x"/></a>
             </div>
-            <div className="team">
-                <h3 className="footer-title">Team Members</h3>
-                <p>Jishnu V</p>
-                <p>Joselyn Diana Cindrella M</p>
-                <p>Kabilan A</p>
-                <p>Kali Deepak T</p>
-                <p>Nivedhidha I</p>
-            </div>
         </footer>
     </div>
     )
