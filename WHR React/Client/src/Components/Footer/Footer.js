2b5f71c6ca8634dfe7c748e814b7055a1e96b9a6
Nivedhidha-I <71599134+Nivedhidha-I@users.noreply.github.com>
2026-01-29 22:55:00 +0100

    Update Footer.js

--- WHR React/Client/src/Components/Footer/Footer.js
+++ WHR React/Client/src/Components/Footer/Footer.js
@@ -3,12 +3,10 @@ function Footer(){
     return (
     <div>
         <footer className="footer-grid">
-            <div className="links">
-                <h3 className="footer-title">References</h3>
-            </div>
+            <div className="links"></div>
         <div className="links">
-            <a href="https://github.com/Nivedhidha-I/World-Happiness-Report"><i class="icons fab fa-github fa-3x"/></a>
-            <a href="https://www.kaggle.com/mathurinache/world-happiness-report?select=2020.csv"><i class="icons fab fa-kaggle fa-3x"/></a>
+            <a href="https://github.com/Nivedhidha-I/World-Happiness-Report"><i class="icons fab fa-github"/></a>
+            <a href="https://www.kaggle.com/mathurinache/world-happiness-report?select=2020.csv"><i class="icons fab fa-kaggle"/></a>
         </div>
         </footer>
     </div>
